c2811cddb730021bc5873a390bee722cd67c76ee
Shreeda Ghatpande <shreeda3@gmail.com>
2025-09-04 14:22:10 -0600

    Added shardIdx cache logic it is giving mixed results

--- kvs/server/main.go
+++ kvs/server/main.go
@@ -14,7 +14,56 @@ import (
 	"github.com/rstutsman/cs6450-labs/kvs"
 )
 
-var numShards uint32
+var numShards uint64
+var enableCache bool
+
+type KeyHash struct {
+	shardIdx uint64
+	keyHash  uint64
+}
+type CacheKeyHasher struct {
+	muCache sync.RWMutex // lock for concurrent access
+	cache   map[string]*KeyHash
+}
+
+func newCache() *CacheKeyHasher {
+	return &CacheKeyHasher{
+		cache: make(map[string]*KeyHash, 100_000),
+	}
+}
+
+func (c *CacheKeyHasher) getShardIndex(k string) *KeyHash {
+	c.muCache.RLock()
+	kHash, found := c.cache[k]
+	c.muCache.RUnlock()
+	if found {
+		return kHash
+	} else {
+		h := fnv.New64a()
+		h.Write([]byte(k))
+		keyHash := h.Sum64()
+		kHash := &KeyHash{shardIdx: uint64(keyHash % numShards), keyHash: keyHash}
+		c.muCache.Lock()
+		c.cache[k] = kHash
+		c.muCache.Unlock()
+		return kHash
+	}
+
+}
+
+var keyHasherCache = newCache()
+
+func getShardIndexCached(k string) *KeyHash {
+	if enableCache {
+		return keyHasherCache.getShardIndex(k)
+	} else {
+		h := fnv.New64a()
+		h.Write([]byte(k))
+		keyHash := h.Sum64()
+		return &KeyHash{(keyHash % numShards), keyHash}
+
+	}
+}
 
 type Stats struct {
 	puts uint64
@@ -30,29 +79,27 @@ func (s *Stats) Sub(prev *Stats) Stats {
 
 type Shard struct {
 	muShard sync.RWMutex
-	mp      map[string]string
+	mp      map[uint64]string
 }
 
 type ShardMap struct {
-	shards map[uint32]*Shard
+	shards map[uint64]*Shard
 }
 
 // Constructor
-func NewShardedMap(shardCount uint32, mapAllocCount uint64) *ShardMap {
-	m := &ShardMap{shards: make(map[uint32]*Shard, shardCount)}
-	for i := uint32(0); i < shardCount; i++ {
+func NewShardedMap(shardCount uint64, mapAllocCount uint64) *ShardMap {
+	m := &ShardMap{shards: make(map[uint64]*Shard, shardCount)}
+	for i := uint64(0); i < shardCount; i++ {
 		m.shards[i] = &Shard{
-			mp: make(map[string]string, mapAllocCount),
+			mp: make(map[uint64]string, mapAllocCount),
 		}
 	}
 	return m
 }
 
-func getShardIndex(key string) uint32 {
-	h := fnv.New32a()
-	h.Write([]byte(key))
-	return h.Sum32() % numShards
-}
+// func getShardIndex(key string) (uint64, uint64) {
+
+// }
 
 type KVService struct {
 	muStatsGets sync.Mutex
@@ -63,7 +110,7 @@ type KVService struct {
 	lastPrint   time.Time
 }
 
-func NewKVService(shardCount uint32, mapAllocCount uint64) *KVService {
+func NewKVService(shardCount uint64, mapAllocCount uint64) *KVService {
 	kvs := &KVService{}
 	kvs.shardmp = NewShardedMap(shardCount, mapAllocCount)
 	kvs.lastPrint = time.Now()
@@ -79,10 +126,10 @@ func (kv *KVService) Get(request *kvs.GetRequest, response *kvs.GetResponse) err
 	resValue := ""
 	for _, key := range request.Key {
 		resValue = ""
-		idx := getShardIndex(key)
-		sh := kv.shardmp.shards[idx]
+		kHash := getShardIndexCached(key)
+		sh := kv.shardmp.shards[kHash.shardIdx]
 		sh.muShard.RLock()
-		val, found := sh.mp[key]
+		val, found := sh.mp[kHash.keyHash]
 		sh.muShard.RUnlock()
 		if found {
 			resValue = val
@@ -100,10 +147,10 @@ func (kv *KVService) Put(request *kvs.PutRequest, response *kvs.PutResponse) err
 	kv.stats.puts++
 	kv.muStatsPuts.Unlock()
 
-	idx := getShardIndex(request.Key)
-	sh := kv.shardmp.shards[idx]
+	kHash := getShardIndexCached(request.Key)
+	sh := kv.shardmp.shards[kHash.shardIdx]
 	sh.muShard.Lock()
-	sh.mp[request.Key] = request.Value
+	sh.mp[kHash.keyHash] = request.Value
 	sh.muShard.Unlock()
 
 	return nil
@@ -132,10 +179,10 @@ func (kv *KVService) printStats() {
 
 func main() {
 	port := flag.String("port", "8080", "Port to run the server on")
-	shardCount := *flag.Int("num-shards", 64, "Number of Shards in the KVStore")
+	numShards = *flag.Uint64("num-shards", 64, "Number of Shards in the KVStore")
 	mapAllocCount := *flag.Uint64("alloc", 400_000, "Number expected for keys per shard")
+	enableCache = *flag.Bool("cache", false, "Use cached values for string storage")
 	flag.Parse()
-	numShards = uint32(shardCount)
 
 	kvs := NewKVService(numShards, mapAllocCount)
 	rpc.Register(kvs)
@@ -146,7 +193,7 @@ func main() {
 		log.Fatal("listen error:", e)
 	}
 
-	fmt.Printf("Starting KVS server on :%s\n", *port)
+	fmt.Printf("Starting KVS server on :%s %t\n", *port, enableCache)
 
 	go func() {
 		for {
